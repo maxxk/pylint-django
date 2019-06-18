055dcdf1d9b1e5336b07b55b8ae4f9db33181c30
Maxim Krivchikov <maxim.krivchikov@gmail.com>
2019-06-18 17:02:57 +0300

    use better approximation for QuerySet methods

--- pylint_django/transforms/transforms/django_db_models.py
+++ pylint_django/transforms/transforms/django_db_models.py
@@ -6,10 +6,12 @@ def __noop(self, *args, **kwargs):
     """Just a dumb no-op function to make code a bit more DRY"""
     return None
 
-
 def __noop_list(self, *args, **kwargs):
-    """Just a dumb no-op function to make code a bit more DRY"""
     return []
+
+def __noop_manager(self, *args, **kwargs):
+    """Just a dumb no-op function to make code a bit more DRY"""
+    return SubscriptableManager()
 
 
 class Model(object):
@@ -29,34 +31,48 @@ class Manager(object):
     """
     Eliminate E1002 for Manager object
     """
-    get_queryset = __noop
+
+    def get_or_create(self, *args, **kwargs):
+        return None, False
+    
+    def aggregate(self, *args, **kwargs):
+        return {}
+    
+    def in_bulk(self, id_list=None):
+        return {}
+
+    get_queryset = __noop_manager
     none = __noop
-    all = __noop_list
+    all = __noop_manager
     count = __noop
     dates = __noop
-    distinct = __noop
-    extra = __noop
+    distinct = __noop_manager
+    extra = __noop_manager
     get = __noop
-    get_or_create = __noop
     create = __noop
     bulk_create = __noop
-    filter = __noop_list
-    aggregate = __noop
-    annotate = __noop
+    filter = __noop_manager
+    annotate = __noop_manager
     complex_filter = __noop
-    exclude = __noop
-    in_bulk = __noop
+    exclude = __noop_manager
     iterator = __noop
     latest = __noop
-    order_by = __noop
+    order_by = __noop_manager
     select_for_update = __noop
-    select_related = __noop
-    prefetch_related = __noop
-    values = __noop
-    values_list = __noop
+    select_related = __noop_manager
+    prefetch_related = __noop_manager
+    values = __noop_manager
+    values_list = __noop_manager
     update = __noop
-    reverse = __noop
-    defer = __noop
-    only = __noop
-    using = __noop
+    reverse = __noop_manager
+    defer = __noop_manager
+    only = __noop_manager
+    using = __noop_manager
     exists = __noop
+
+    __contains__ = __noop
+    __iter__ = __noop_list
+
+class SubscriptableManager(list, Manager):
+    pass
+
